fd37b3f4c3568616c36566b24f6cd16b1e0cf194
Alvis-Apins <alvis.apins.it@gmail.com>
2022-06-16 14:55:01 +0100

    Add files via upload

--- arrays/05-TicTacToe.php
+++ arrays/05-TicTacToe.php
@@ -1,57 +1,76 @@
 <?php
 ## Exercise #5
-function createArray(int $size): array
-{
-    return array_fill(0, $size, ' ');
-}
-$emptyGameBoard = createArray(9);
+//Create an interactive game of Tic-Tac-Toe
 
-for ($i = 1; $i <= 9; $i++)
-{
-    echo $emptyGameBoard[0] . '  |  ' . $emptyGameBoard[1] . '  |  ' . $emptyGameBoard[2] . PHP_EOL;
-    echo '---------------' . PHP_EOL;
-    echo $emptyGameBoard[3] . '  |  ' . $emptyGameBoard[4] . '  |  ' . $emptyGameBoard[5] . PHP_EOL;
-    echo '---------------' . PHP_EOL;
-    echo $emptyGameBoard[6] . '  |  ' . $emptyGameBoard[7] . '  |  ' . $emptyGameBoard[8] . PHP_EOL;
+$emptyGameBoard = array_fill(0, 9, ' ');
+$playerTurn = 'X';
+
+function checkWinner(array $array):bool{
+    if (($array[0] == $array[1] && $array[1] == $array[2] && $array[0] != ' ') ||
+        ($array[3] == $array[4] && $array[4] == $array[5] && $array[3] != ' ') ||
+        ($array[6] == $array[7] && $array[7] == $array[8] && $array[6] != ' ') ||
+
+        ($array[0] == $array[3] && $array[3] == $array[6] && $array[0] != ' ') ||
+        ($array[1] == $array[4] && $array[4] == $array[7] && $array[1] != ' ') ||
+        ($array[2] == $array[5] && $array[5] == $array[8] && $array[2] != ' ') ||
+
+        ($array[0] == $array[4] && $array[4] == $array[8] && $array[0] != ' ') ||
+        ($array[6] == $array[4] && $array[4] == $array[2] && $array[6] != ' '))
+    {
+        return true;
+    }
+    return false;
+}
+
+function gameMechanics(string $playerTurn, array $emptyGameBoard){
+
+    $winner = ' ';
+
+    while ($winner == ' '){
+
+        echo $emptyGameBoard[0] . '  |  ' . $emptyGameBoard[1] . '  |  ' . $emptyGameBoard[2] . PHP_EOL;
+        echo '---------------' . PHP_EOL;
+        echo $emptyGameBoard[3] . '  |  ' . $emptyGameBoard[4] . '  |  ' . $emptyGameBoard[5] . PHP_EOL;
+        echo '---------------' . PHP_EOL;
+        echo $emptyGameBoard[6] . '  |  ' . $emptyGameBoard[7] . '  |  ' . $emptyGameBoard[8] . PHP_EOL;
+        echo PHP_EOL;
+
+        $a = readline("Where would you like to place $playerTurn? :");
+        echo PHP_EOL;
+
+        if ($a >= 0 && $a < 9 && $emptyGameBoard[$a] == ' '){
+            $emptyGameBoard[$a] = $playerTurn;
+        }else{
+            echo "!!!Not a valid option - Play by the rules!!!" . PHP_EOL;
+        }
+
+        if (checkWinner($emptyGameBoard)){
+            $winner = "$playerTurn Wins!";
+        }
+
+        if (!in_array(' ', $emptyGameBoard)){
+            $winner = "Draw";
+        }
+
+        ($playerTurn == 'O') ? $playerTurn = 'X' : $playerTurn = 'O';
+
+    }
     echo PHP_EOL;
-    echo ($i % 2 != 0) ?
-        'Turn ' . $i . ' for player X ' . PHP_EOL :
-        'Turn ' . $i . ' for player O ' . PHP_EOL;
+    echo $winner . PHP_EOL;
 
-    $a = readline('Pick your field - ' . PHP_EOL);
+    $reStarter = readline('Start a blank game? (y/n)');
+    echo PHP_EOL;
 
-    if ($a >= 0 && $a < 9)
-    {
-        ($i % 2 != 0) ? $emptyGameBoard[$a] = 'X' : $emptyGameBoard[$a] = 'O';
-    }else
-    {
-        echo PHP_EOL . '!!! Please select a number between 0 and 8 !!!' . PHP_EOL;
-        continue;
-    }
-
-    if ($i == 9)
-    {
-        echo 'Draw' . PHP_EOL;
-        return;
-    }
-
-    if (($emptyGameBoard[0] == $emptyGameBoard[1] && $emptyGameBoard[1] == $emptyGameBoard[2] && $emptyGameBoard[0] != ' ') ||
-        ($emptyGameBoard[3] == $emptyGameBoard[4] && $emptyGameBoard[4] == $emptyGameBoard[5] && $emptyGameBoard[3] != ' ') ||
-        ($emptyGameBoard[6] == $emptyGameBoard[7] && $emptyGameBoard[7] == $emptyGameBoard[8] && $emptyGameBoard[6] != ' ') ||
-
-        ($emptyGameBoard[0] == $emptyGameBoard[3] && $emptyGameBoard[3] == $emptyGameBoard[6] && $emptyGameBoard[0] != ' ') ||
-        ($emptyGameBoard[1] == $emptyGameBoard[4] && $emptyGameBoard[4] == $emptyGameBoard[7] && $emptyGameBoard[1] != ' ') ||
-        ($emptyGameBoard[2] == $emptyGameBoard[5] && $emptyGameBoard[5] == $emptyGameBoard[8] && $emptyGameBoard[2] != ' ') ||
-
-        ($emptyGameBoard[0] == $emptyGameBoard[4] && $emptyGameBoard[4] == $emptyGameBoard[8] && $emptyGameBoard[0] != ' ') ||
-        ($emptyGameBoard[6] == $emptyGameBoard[4] && $emptyGameBoard[4] == $emptyGameBoard[2] && $emptyGameBoard[6] != ' '))
-    {
-        echo ($i % 2 != 0) ? 'Player X wins' . PHP_EOL : 'Player O wins' . PHP_EOL;
+    if ($reStarter == 'y'){
+        $emptyGameBoard = array_fill(0, 9, ' ');
+        $winner = ' ';
+        gameMechanics($playerTurn, $emptyGameBoard);
+    }else{
         return;
     }
 }
 
-
+gameMechanics($playerTurn, $emptyGameBoard);
 
 
 
